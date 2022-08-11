3d268f3883fac2a0a8b23a6e3a3b8951d50a7aa1
olamilekan <muritala207@gmail.com>
2022-08-11 16:15:24 -0700

    great todo app

--- src/practical.js
+++ src/practical.js
@@ -8,39 +8,81 @@ export default function Practical() {
     e.preventDefault();
     setask(e.target.value);
   }
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {
-        console.log(res);
-        setpost(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  function addtext() {
+    if (task !== "") {
+      const element = {
+        id: Math.floor(Math.random() * 1000),
+        value: task,
+        isCompleted: false,
+      };
+      setpost([...post, element]);
+      console.log(setpost([...post, element]));
+    }
+  }
+  function deletes(event, id) {
+    event.preventDefault();
+    setpost(post.filter((prev) => prev.id != id));
+  }
+
+  function completed(elem, id) {
+    elem.preventDefault();
+    const element = post.findIndex((prev) => prev.id == id);
+    const news = [...post];
+    news[element] = {
+      ...news[element],
+      isCompleted: true,
+    };
+    setpost(news);
+  }
+  function clear(e) {
+    e.preventDefault();
+    setpost([]);
+  }
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="search....."
-        onChange={settype}
-        className="text"
-      />
-      <br />
       <div>
-        {post
-          .filter((prev) => prev.title.toLowerCase().includes(task))
-          .map((prev) => {
-            return (
-              <ul key={prev.id}>
-                <li key={prev.id} className="listed">
-                  {prev.title}
-                </li>
-              </ul>
-            );
-          })}
+        <input
+          type="text"
+          onChange={settype}
+          placeholder="what are you craving for ?!"
+          className="input"
+        />
+        <button className="add-btn" onClick={addtext}>
+          Add
+        </button>
       </div>
+      <div>
+        {post != [] ? (
+          <ul>
+            {post.map((prev) => (
+              <li
+                key={prev.id}
+                className={prev.isCompleted ? "styless" : "list-item"}
+              >
+                {prev.value}
+                <div className="join">
+                  <button
+                    className="delete-btn"
+                    onClick={(e) => deletes(e, prev.id)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className="completed"
+                    onClick={(e) => completed(e, prev.id)}
+                  >
+                    Completed
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+      <button className="clear" onClick={clear}>
+        Clear
+      </button>
     </div>
   );
 }
