7adfb09ceb135294c9a2b2670bdf87b1d0d5b0c8
olamilekan <muritala207@gmail.com>
2022-08-14 17:01:38 -0700

    second project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Practical from "./practical";
 function App() {
   return (
     <div className="certified">
-      <h1 className="head"> WONDER todo app</h1>
+      <h1 className="head"> WONDER search engine</h1>
       <div>
         <Practical />
       </div>
--- src/practical.js
+++ src/practical.js
@@ -1,88 +1,48 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-export default function Practical() {
+
+function Practical() {
   const [task, setask] = useState("");
-  const [post, setpost] = useState([]);
-  function settype(e) {
-    e.preventDefault();
-    setask(e.target.value);
-  }
-  function addtext() {
-    if (task !== "") {
-      const element = {
-        id: Math.floor(Math.random() * 1000),
-        value: task,
-        isCompleted: false,
-      };
-      setpost([...post, element]);
-      console.log(setpost([...post, element]));
-    }
-  }
-  function deletes(event, id) {
+  const [tasklist, setasklist] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        console.log(res);
+        setasklist(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  function setinput(event) {
     event.preventDefault();
-    setpost(post.filter((prev) => prev.id != id));
+    setask(event.target.value);
   }
-
-  function completed(elem, id) {
-    elem.preventDefault();
-    const element = post.findIndex((prev) => prev.id == id);
-    const news = [...post];
-    news[element] = {
-      ...news[element],
-      isCompleted: true,
-    };
-    setpost(news);
-  }
-  function clear(e) {
-    e.preventDefault();
-    setpost([]);
-  }
-
   return (
     <div>
+      <input
+        type="text"
+        className="input"
+        onChange={setinput}
+        placeholder="search...."
+      />
       <div>
-        <input
-          type="text"
-          onChange={settype}
-          placeholder="what are you craving for ?!"
-          className="input"
-        />
-        <button className="add-btn" onClick={addtext}>
-          Add
-        </button>
+        <ul>
+          {tasklist
+            .filter((prev) => prev.title.toLowerCase().includes(task))
+            .map((prev) => {
+              return (
+                <li key={prev.id} className="listed">
+                  {prev.title}
+                </li>
+              );
+            })}
+        </ul>
       </div>
-      <div>
-        {post != [] ? (
-          <ul>
-            {post.map((prev) => (
-              <li
-                key={prev.id}
-                className={prev.isCompleted ? "styless" : "list-item"}
-              >
-                {prev.value}
-                <div className="join">
-                  <button
-                    className="delete-btn"
-                    onClick={(e) => deletes(e, prev.id)}
-                  >
-                    Delete
-                  </button>
-                  <button
-                    className="completed"
-                    onClick={(e) => completed(e, prev.id)}
-                  >
-                    Completed
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : null}
-      </div>
-      <button className="clear" onClick={clear}>
-        Clear
-      </button>
     </div>
   );
 }
+
+export default Practical;
